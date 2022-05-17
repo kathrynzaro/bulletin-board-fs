842c61c03aa3e74b446ac009649f8539f4290966
kathrynzaro <zarokathryn@gmail.com>
2022-05-17 13:20:39 -0700

    TDD renderBulletinPost function passing

---
+++ render-utils.js
@@ -0,0 +1,16 @@
+export function renderBulletinPost(post) {
+    const div = document.createElement('div');
+    const h2 = document.createElement('h2');
+    const p = document.createElement('p');
+    const h3 = document.createElement('h3');
+
+    div.classList.add('bulletin-post');
+
+    h2.textContent = post.title;
+    p.textContent = post.description;
+    h3.textContent = post.author;
+
+    div.append(h2, p, h3);
+
+    return div;
+}
--- test/example.test.js
+++ test/example.test.js
@@ -1,18 +1,26 @@
 // IMPORT MODULES under test here:
 // import { example } from '../example.js';
 
+import { renderBulletinPost } from "../render-utils.js";
+
 const test = QUnit.test;
 
-test('time to test a function', (expect) => {
+test('renderBulletinPost renders a div with post details', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = true;
+    const expected = `<div class="bulletin-post"><h2>hello</h2><p>looking for a void to hang out in for the next 50 years</p><h3>plot twist it me</h3></div>`;
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = true;
+    const hello = {
+        id: 3,
+        title: 'hello',
+        description: 'looking for a void to hang out in for the next 50 years',
+        author: 'plot twist it me',
+    };
+    const actual = renderBulletinPost(hello);
 
     //Expect
     // Make assertions about what is expected versus the actual result
-    expect.equal(actual, expected);
+    expect.equal(actual.outerHTML, expected);
 });
