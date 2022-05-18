e7ff1d6950fee74d9faff6a9b2650830caca1f67
kathrynzaro <zarokathryn@gmail.com>
2022-05-18 12:12:00 -0700

    adding create page, new post form, event listener

---
+++ create/create.js
@@ -0,0 +1,14 @@
+
+
+const form = document.getElementById('new-post');
+
+form.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const data = new FormData(form);
+    const newPost = {
+        title: data.get('title'),
+        description: data.get('description'),
+        author: data.get('author'),
+    };
+    
+})
